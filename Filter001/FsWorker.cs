d634833421dfca151c55c228f858314e3637336c
Alex <aleksandrpermakov105@gmail.com>
2025-04-28 11:47:23 +0500

    набросок делегата

---
+++ Filter001/FsWorker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filter001
+{
+    delegate string myFilter(string text);
+    public class FsWorker
+    {
+        static public bool AppendString(myFilter name)
+        {
+            string str = name;
+            try
+            {
+                using (StreamWriter sw = new StreamWriter("out.txt", true))
+                {
+                    sw.WriteLine(_str);
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
